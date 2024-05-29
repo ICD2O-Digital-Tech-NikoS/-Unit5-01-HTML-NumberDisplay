10da6753c0373e2df92d684089cd446194bae4a9
NikoShivkumar <159164670+NikoShivkumar@users.noreply.github.com>
2024-05-29 15:01:06 +0000

    u

--- js/script.js
+++ js/script.js
@@ -8,11 +8,13 @@ function numberDisplay() {
 
   let userMax = document.getElementById("userMax").value;
   let userMin = document.getElementById("userMin").value;
+  let numbers = "";
   let counter = userMin;
 
   while (counter <= userMax) {
+    numbers = numbers + counter + "<br>";
     counter = counter++;
   }
-  document.getElementById("user-info").innerHTML = counter.toString() + "<br>";
+  document.getElementById("user-info").innerHTML = numbers;
 }
 
