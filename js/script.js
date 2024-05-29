f5ec5d4e88020f1d50534726ffac9007bd5eb238
NikoShivkumar <159164670+NikoShivkumar@users.noreply.github.com>
2024-05-29 20:58:26 +0000

    u

--- js/script.js
+++ js/script.js
@@ -18,10 +18,14 @@ function numberDisplay() {
 
   // while counter is not equal to userMax, update the counter unti it is equal to userMax
   while (counter <= userMax) {
-    numbers = numbers + counter + "<br>";
+    numbers = numbers + counter + " ";
     counter++;
   }
   // display the numbers
   document.getElementById("user-info").innerHTML = numbers;
+
+  if (userMin > userMax) {
+    document.getElementById("user-info").innerHTML = "Your minimum is greater than your maximum. Please try again.";
+  }
 }
 
