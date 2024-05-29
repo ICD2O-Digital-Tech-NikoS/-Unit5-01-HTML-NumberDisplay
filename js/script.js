445cc97558cad51b462747f3238f96a5e38e2598
NikoShivkumar <159164670+NikoShivkumar@users.noreply.github.com>
2024-05-29 14:52:26 +0000

    u

--- js/script.js
+++ js/script.js
@@ -1,5 +1,18 @@
-// Copyright (c) 2020 Mr. Coxall All rights reserved
-//
-// Created by: Mr. Coxall
-// Created on: Sep 2020
-// This file contains the JS functions for index.html
+// created by Niko 
+// created on: May 27 2024
+// user number display
+
+"use strict"
+
+function numberDisplay() {
+
+  let userMax = document.getElementById("userMax").value;
+  let userMin = document.getElementById("userMin").value;
+  let counter = userMin;
+
+  while (counter <= userMax) {
+    counter = counter++;
+  }
+  document.getElementById("user-info").innerHTML = counter.toString() + "<br>";
+}
+
